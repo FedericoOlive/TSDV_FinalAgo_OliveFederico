e163fb61c8b4fc334d630c9d647420bf29a0e420
Federico Olive <FedericoOlive@gmail.com>
2021-07-27 04:58:30 -0300

    Add Turret rotation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,40 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Numerics;
 using UnityEngine.Assertions.Must;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
-    [Serializable]
-    public class Tank
+    public Transform bulletGroup;
+    [Serializable] public class Tank
     {
         public GameObject tank;
         public GameObject cannon;
         public GameObject firePoint;
         public GameObject PfBullet;
     }
-
-    [Serializable]
-    public class Attributes
+    [Serializable] public class Settings
     {
-        public float tankSeparateTerrain;
         public float speedTankMovement;
-        public float speedTankRotate;
+        public float rotateTank;
         public float gravityTankRotate;
-        public float speedCannonRotate;
+        public float rotateCannon;
     }
 
+    public float tankSeparateTerrain = 0.5f;
+    public float rateFire;
+    private float rateFireTime;
+    private bool shooting;
+    private bool reloaded;
+
+    public float offsetRangeShoot;
+    public Vector3 pointToShoot;
     [SerializeField] private Tank tank;
-    [SerializeField] private Attributes attributes;
+    [SerializeField] private Settings settings;
 
     void Start()
     {
@@ -34,72 +43,105 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        AutoRotation();
-        if (Input.GetMouseButtonDown(0))
-        {
-            /*
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            StartCoroutine(RotateCannon(mousePos));*/
-        }
+        TryShoot();
     }
-
     void FixedUpdate()
     {
         Movement();
         Rotation();
+        AutoRotation();
         FixHeightOnTerrain();
     }
-
-    IEnumerator RotateCannon(Vector3 objetive)
+    void TryShoot()
     {
-        while (false)
+        rateFireTime += Time.deltaTime;
+        if (rateFireTime > rateFire)
         {
-            yield return null;
+            if (!reloaded)
+            {
+                // Evento recargado
+            }
+            reloaded = true;
+            if (Input.GetMouseButtonDown(0) && !shooting && reloaded)
+            {
+                reloaded = false;
+                shooting = true;
+                Aim();
+            }
         }
     }
+    private void Aim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.black);
+        RaycastHit hit;
 
+        if (Physics.Raycast(ray, out hit))
+        {
+            StartCoroutine(Shooting(hit.point));
+        }
+    }
+    IEnumerator Shooting(Vector3 point)                         //todo: clampear la rotacion en z a 0 y de x entre 9 y -20
+    {
+        Transform cannonT = tank.cannon.transform;
+        Quaternion rotateDestiny = Quaternion.LookRotation(point - cannonT.position, cannonT.up);
+        
+        float posX = Mathf.Clamp(rotateDestiny.eulerAngles.x, -20, 9);
+        rotateDestiny = Quaternion.Euler(posX, rotateDestiny.eulerAngles.y, 0);
+        Debug.Log("Clamped: " + rotateDestiny);
+
+        while (cannonT.rotation != rotateDestiny)                                                        
+        {
+            rotateDestiny = Quaternion.LookRotation(point - cannonT.position, cannonT.up);
+            cannonT.rotation = Quaternion.RotateTowards(cannonT.rotation, rotateDestiny, settings.rotateCannon * Time.deltaTime);
+            yield return null;
+        }
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        shooting = false;
+        if (!bulletGroup)
+        {
+            bulletGroup = GameObject.Find("BulletGroup_Ref").transform;
+            Debug.LogWarning("bulletGroup no estaba asignado", gameObject);
+        }
+
+        GameObject bullet = Instantiate(tank.PfBullet, tank.firePoint.transform.position, Quaternion.identity, bulletGroup);
+    }
     void Movement()
     {
         float ver = Input.GetAxisRaw("Vertical");
-        transform.Translate(Vector3.forward * (attributes.speedTankMovement * ver));
-        transform.Translate(Vector3.forward * (-attributes.speedTankMovement / 2 * ver));
+        transform.Translate(Vector3.forward * (settings.speedTankMovement * ver));
+        transform.Translate(Vector3.forward * (-settings.speedTankMovement / 2 * ver));
     }
-
     void Rotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.up, -attributes.speedTankRotate);
+            transform.Rotate(Vector3.up, -settings.rotateTank);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.up, attributes.speedTankRotate);
+            transform.Rotate(Vector3.up, settings.rotateTank);
         }
     }
-
-    Vector3 ProjectPointOnPlane(Vector3 planeNormal, Vector3 planePoint, Vector3 point)
-    {
-        planeNormal.Normalize();
-        var distance = -Vector3.Dot(planeNormal, (point - planePoint));
-        return point + planeNormal * distance;
-    }
-
     void AutoRotation()
     {
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit))
         {
             Quaternion qTo = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-            transform.rotation = Quaternion.Slerp(transform.rotation, qTo, attributes.gravityTankRotate * Time.deltaTime);
-            transform.rotation = Quaternion.Slerp(transform.rotation, qTo, attributes.speedTankRotate * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, qTo, settings.gravityTankRotate * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, qTo, settings.rotateTank * Time.deltaTime);
         }
     }
-
     void FixHeightOnTerrain()
     {
         Vector3 newpos = transform.position;
-        newpos.y = Terrain.activeTerrain.SampleHeight(transform.position) + attributes.tankSeparateTerrain;
+        newpos.y = Terrain.activeTerrain.SampleHeight(transform.position) + tankSeparateTerrain;
         transform.position = newpos;
     }
 }
